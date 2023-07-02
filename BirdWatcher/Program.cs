599fd3f1b405a917b67c3b262204adb54ec24b2e
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2023-07-02 02:23:17 -0400

    All done by now

---
+++ BirdWatcher/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+         var counts = new int[]
+        {
+            0,
+            0,
+            1,
+            0,
+            0,
+            1,
+            0
+        };
+
+            var birdCount = new BirdCount(counts);
+            var res = birdCount.CountForFirstDays(3);
+            Console.WriteLine(res);
+        }
+    }
+
+	class BirdCount
+	{
+
+        private int[] birdsPerDay;
+
+        public BirdCount(int[] birdsPerDay)
+        {
+            this.birdsPerDay = birdsPerDay;
+        }
+
+        public static int[] LastWeek()
+        {
+            int[] lastWeek= new int[] {0,2,5,3,7,8,4 };
+            return lastWeek;
+        }
+
+        public int Today()
+        {
+          var length =  birdsPerDay.Length;
+
+            return birdsPerDay[length - 1];
+
+        }
+
+        public void IncrementTodaysCount()
+        {
+           
+            var length = birdsPerDay.Length;
+            var last = birdsPerDay[length - 1];
+            var Incremented = last+= 1;
+
+            birdsPerDay[length - 1] = Incremented;
+
+            
+ 
+        }
+
+        public bool HasDayWithoutBirds()
+        {
+            for(int i = 0; i < birdsPerDay.Length; i++)
+			{
+               if( birdsPerDay[i] == 0)
+				{
+                    return true;
+				}
+        
+			}
+        return false;
+        }
+
+        public int CountForFirstDays(int numberOfDays)
+        {
+            var sum = 0;
+            if(numberOfDays <= birdsPerDay.Length - 1) { 
+            for(int i =0; i < numberOfDays; i++)
+			{
+                    sum += birdsPerDay[i];
+                 
+			}
+            
+           }
+
+            return sum;
+        }
+
+        public int BusyDays()
+        {
+            var busyDays = 0;
+            foreach(var birds in birdsPerDay)
+			{
+                if(birds >= 5)
+				{
+                    busyDays+= 1;
+				}
+			}
+            return busyDays;
+            
+        }
+
+    }
+}
---
+++ Elon'sToys/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elon_sToys
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            var res1 = RemoteControlCar.Buy();
+
+			Console.WriteLine(res1.BatteryDisplay());
+			Console.WriteLine(res1.DistanceDisplay());
+
+            res1.Drive();
+            res1.Drive();
+
+            Console.WriteLine(res1.BatteryDisplay());
+            Console.WriteLine(res1.DistanceDisplay());
+
+            var car = new RemoteControlCar();
+            for (var i = 0; i < 100; i++)
+            {
+                car.Drive();
+            }
+
+            car.Drive();
+			Console.WriteLine(car.BatteryDisplay());
+			Console.WriteLine(car.DistanceDisplay());
+        }
+
+	}
+
+	class RemoteControlCar
+    {
+        private int meterDriven = 0;
+        private int battery = 100;
+        public static RemoteControlCar Buy()
+        {
+
+            RemoteControlCar car = new RemoteControlCar();
+            return car;
+        }
+
+        public string DistanceDisplay()
+        {
+ 
+            return $"Driven {meterDriven} meters" ;
+        }
+
+        public string BatteryDisplay()
+        {
+            if(battery != -1)
+			{
+            return $"Battery at {battery}%";
+			}
+			else
+			{
+                return $"Battery empty";
+            }
+            
+        }
+
+        public void Drive()
+        {
+            meterDriven += 20;
+            battery -= 1;
+            if(battery == -1)
+			{
+                meterDriven -= 20;
+			}
+        }
+
+
+    }
+}
